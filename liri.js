3d6044ad60296ba3c3b73d86872f7d7a747fca5f
Cabaret Voltaire <medinaisrael_17@yahoo.com>
2019-03-26 16:47:07 -0500

    fomat for how liri will run

--- liri.js
+++ liri.js
@@ -2,4 +2,32 @@ require("dotenv").config();
 
 var keys = require('./keys.js');
 
+var response = process.argv[2];
+
 var spotify = new Spotify(keys.spotify);
+
+function concert_this();
+
+function spotify_this_song();
+
+function movie_this();
+
+function do_what_it_says();
+
+if (response === "concert-this") {
+    concert_this();
+
+}
+if (response === "spotify-this-song") {
+    spotify_this_song();
+    
+}
+if (response === "movie-this") {
+    movie_this();
+    
+}
+if (response === "do-what-it-says") {
+    do_what_it_says();
+    
+}
+
