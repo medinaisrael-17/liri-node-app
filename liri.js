a063489d15928d2974dd9f22b08a5442c88cb637
Cabaret Voltaire <medinaisrael_17@yahoo.com>
2019-03-26 17:02:50 -0500

    concert this function

--- liri.js
+++ liri.js
@@ -1,12 +1,23 @@
 require("dotenv").config();
 
+var chalk = require("chalk");
+
+var axios = require("axios");
+
 var keys = require('./keys.js');
 
 var response = process.argv[2];
 
 var spotify = new Spotify(keys.spotify);
 
-function concert_this();
+function concert_this(artist);
+    queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+    axios.get(queryUrl).then(function(response) {
+        console.log(response)
+    }).catch(function(err){
+        console.log(err);
+    });
+
 
 function spotify_this_song();
 
@@ -15,7 +26,7 @@ function movie_this();
 function do_what_it_says();
 
 if (response === "concert-this") {
-    concert_this();
+    concert_this(process.argv[3]);
 
 }
 if (response === "spotify-this-song") {
